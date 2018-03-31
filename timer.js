7cf8ef323dfdc7eba897d8410fdcc0022200469e
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2018-03-31 17:01:22 +0200

    Save/Load on local storage

--- timer.js
+++ timer.js
@@ -1,5 +1,17 @@
 (function () {
 
+  const save = (state) => {
+    const json = JSON.stringify(state);
+    localStorage.setItem('jstimer', json);
+    return state;
+  };
+
+  const load = (id) => {
+    const json = localStorage.getItem('jstimer');
+    const state = JSON.parse(json);
+    return state;
+  };
+
   const pad = (a, b) => (1e15 + a + "").slice(-b)
 
   const now = () => new Date();
@@ -11,7 +23,7 @@
     return pad(hours.toFixed(0), 2) + ":" + pad(minutes.toFixed(0), 2) + ":" + pad(seconds.toFixed(0), 2);
   }
 
-  const updateState = (state, update) => Object.assign({}, state, update);
+  const updateState = (state, update) => save(Object.assign({}, state, update));
 
   const renderTimeEl = (state) => {
     const timeToTarget = state.playing ? (state.targetTime - now().getTime()) : state.timeDiff;
@@ -20,7 +32,7 @@
 
   const play = (state) => updateState(state, {
     playing: true,
-    targetTime: new Date(now().getTime() + state.timeDiff),
+    targetTime: new Date(now().getTime() + state.timeDiff).getTime(),
   });
 
   const pause = (state) => updateState(state, {
@@ -32,10 +44,9 @@
     const timeDiff = state.timeDiff + milliseconds;
     return updateState(state, {
       timeDiff,
-      targetTime: new Date(now().getTime() + timeDiff),
+      targetTime: new Date(now().getTime() + timeDiff).getTime(),
     });
   };
-
 
   const setup = () => {
     const timeEl = document.getElementById('timer');
@@ -46,11 +57,15 @@
 
     const defaultTimeDiff = 60 * 90 * 1000; // milliseconds
 
-    let state = {
+    const defaultState = {
       playing: false,
       timeDiff: defaultTimeDiff,
-      targetTime: new Date(now().getTime() + defaultTimeDiff),
+      targetTime: new Date(now().getTime() + defaultTimeDiff).getTime(),
     };
+
+    const savedState = load();
+
+    let state = Object.assign({}, defaultState, savedState);
 
     playEl.onclick = () => {
       state = play(state);
@@ -72,7 +87,6 @@
     });
 
     const loop = () => {
-      state = updateState(state);
       timeEl.innerHTML = renderTimeEl(state);
     };
 
